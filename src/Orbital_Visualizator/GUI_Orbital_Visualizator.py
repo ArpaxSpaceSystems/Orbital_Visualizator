4a891ef068393206d1e558cfabea4aeb9af896b3
Baptistestaca <81914248+Baptistestaca@users.noreply.github.com>
2024-12-16 13:19:40 +0100

    Formatting

--- src/Orbital_Visualizator/GUI_Orbital_Visualizator.py
+++ src/Orbital_Visualizator/GUI_Orbital_Visualizator.py
@@ -1,72 +1,103 @@
+from typing import Union
 import tkinter as tk
 from tkinter import messagebox, Text, Entry, StringVar, Tk, Frame, Label
 from skyfield.api import EarthSatellite
 import numpy as np
-from typing import Union, Tuple
 
 
-
-def submit_parameters(option:StringVar, entry_tle:Text, name_tle:Text, entry_param:list[Entry], window:Tk, Orbital_Parameters:list[Union[float, str]]) -> None:
+def submit_parameters(
+    option: StringVar,
+    entry_tle: Text,
+    name_tle: Text,
+    entry_param: list[Entry],
+    window: Tk,
+    orbital_parameters: list[Union[float, str]],
+) -> None:
     """
-        Handles the submission of orbital parameters from the user interface.
+    Handles the submission of orbital parameters from the user interface.
 
-        This method is triggered when the submit button is clicked.
-        This function is triggered when the submit button is clicked. It extracts
-        and validates user inputs from the provided widgets, then updates the global Orbital_Parameters list.
-        Used within the `select_data()` method.
+    This method is triggered when the submit button is clicked.
+    This function is triggered when the submit button is clicked. It extracts
+    and validates user inputs from the provided widgets, then updates the global 
+    Orbital_Parameters list.
+    Used within the `select_data()` method.
 
-        :param option: A `tkinter.StringVar` holding the user's choice between TLE and direct orbital parameters
-        :param entry_tle: A `tkinter.Text` widget containing the two lines element
-        :param name_tle: A `tkinter.Text` widget containing the name of the satellite
-        :param entry_param: A list of `tkinter.Entry` widgets for direct orbital parameters
-        :param window: The `tkinter.Tk` instance of the user interface window.
-        :param Orbital_Parameters: A global list to store the keplerian orbital parameters including TLE data or direct parameters.
-        :return: None. The parameters are directly appended to the `Orbital_Parameters` list.
+    :param option: A `tkinter.StringVar` holding the user's choice between TLE and 
+    direct orbital parameters
+    :param entry_tle: A `tkinter.Text` widget containing the two lines element
+    :param name_tle: A `tkinter.Text` widget containing the name of the satellite
+    :param entry_param: A list of `tkinter.Entry` widgets for direct orbital parameters
+    :param window: The `tkinter.Tk` instance of the user interface window.
+    :param Orbital_Parameters: A global list to store the keplerian orbital parameters including 
+    TLE data or direct parameters.
+    :return: None. The parameters are directly appended to the `Orbital_Parameters` list.
     """
-    mu_Earth = 398600.4418  # km^3 / s^2
+    mu_earth = 398600.4418  # km^3 / s^2
     if option.get() == "TLE":
         tle_line_1 = entry_tle.get("1.0", "1.end").strip()
         tle_line_2 = entry_tle.get("2.0", "2.end").strip()
-        if ((tle_line_1)and(tle_line_2)and(name_tle)):
+        if (tle_line_1) and (tle_line_2) and (name_tle):
             satellite = EarthSatellite(tle_line_1, tle_line_2, name_tle.get())
-            Orbital_Parameters.append((mu_Earth / (2*np.pi/(((2*np.pi/(satellite.model.no_kozai*60))*3600)))**2)**(1/3))
-            Orbital_Parameters.append(satellite.model.ecco)
-            Orbital_Parameters.append(satellite.model.inclo)        #in radian
-            Orbital_Parameters.append(satellite.model.nodeo)        #in radian
-            Orbital_Parameters.append(satellite.model.argpo)        #in radian
-            Orbital_Parameters.append(satellite.model.mo)           #in radian
-            Orbital_Parameters.append(satellite.name)
+            orbital_parameters.append(
+                (
+                    mu_earth
+                    / (
+                        2
+                        * np.pi
+                        / (((2 * np.pi / (satellite.model.no_kozai * 60)) * 3600))
+                    )
+                    ** 2
+                )
+                ** (1 / 3)
+            )
+            orbital_parameters.append(satellite.model.ecco)
+            orbital_parameters.append(satellite.model.inclo)  # in radian
+            orbital_parameters.append(satellite.model.nodeo)  # in radian
+            orbital_parameters.append(satellite.model.argpo)  # in radian
+            orbital_parameters.append(satellite.model.mo)  # in radian
+            orbital_parameters.append(satellite.name)
             window.destroy()
         else:
-            Orbital_Parameters.clear()
+            orbital_parameters.clear()
             messagebox.showwarning("Error", "Please enter a valid TLE and a name.")
     else:
-        
+
         for i in range(6):
             if entry_param[i].get():
-                Orbital_Parameters.append(float(entry_param[i].get()))
+                orbital_parameters.append(float(entry_param[i].get()))
             else:
                 break
-        Orbital_Parameters.append(entry_param[6].get())
-        if all(element for element in Orbital_Parameters):
+        orbital_parameters.append(entry_param[6].get())
+        if all(element for element in orbital_parameters):
             window.destroy()
         else:
-            Orbital_Parameters.clear()
-            messagebox.showwarning("Error", "Please enter valid numerical values for orbital parameters.")
-        
+            orbital_parameters.clear()
+            messagebox.showwarning(
+                "Error", "Please enter valid numerical values for orbital parameters."
+            )
 
-def toggle_fields(option:StringVar, label:Label, entry:Text, label_name:Label, name:Entry, parameters:Frame)->None:    #function to determine visibility of fields depending on the chosen option
+
+def toggle_fields(
+    option: StringVar,
+    label: Label,
+    entry: Text,
+    label_name: Label,
+    name: Entry,
+    parameters: Frame,
+) -> None:  # function to determine visibility of fields depending on the chosen option
     """
-        Manages visibility of fields in the GUI based on the choice made by the user: either TLE or direct orbital parameters.
-        It is used within the `select_data()` method.
+    Manages visibility of fields in the GUI based on the choice made by the user: either 
+    TLE or direct orbital parameters.
+    It is used within the `select_data()` method.
 
-        :param option: A `tkinter.StringVar` containing the user's choice ("TLE" or "direct").
-        :param label: A `tkinter.Label` widget for the TLE input
-        :param entry: A `tkinter.Text` widget where TLE data is entered.
-        :param label_name: A `tkinter.Label` widget for the satellite's name (TLE option).
-        :param name: A `Entry` tkinter.widget for entering the satellite's name (TLE option).
-        :param parameters: A `tkinter.Frame` widget containing all input fields for direct orbital parameters.
-        :return: None. The function directly modifies the visibility of the provided widgets.
+    :param option: A `tkinter.StringVar` containing the user's choice ("TLE" or "direct").
+    :param label: A `tkinter.Label` widget for the TLE input
+    :param entry: A `tkinter.Text` widget where TLE data is entered.
+    :param label_name: A `tkinter.Label` widget for the satellite's name (TLE option).
+    :param name: A `Entry` tkinter.widget for entering the satellite's name (TLE option).
+    :param parameters: A `tkinter.Frame` widget containing all input fields for direct orbital
+     parameters.
+    :return: None. The function directly modifies the visibility of the provided widgets.
     """
     if option.get() == "TLE":
         label.pack(pady=5)
@@ -81,55 +112,102 @@ def toggle_fields(option:StringVar, label:Label, entry:Text, label_name:Label, n
         name.pack_forget()
         parameters.pack(pady=10)
 
-def select_data(Orbital_Parameters: list)->None:              #function to open an user interface to give orbital parameters
-    """
-        Configures and displays the GUI to enter TLE or direct orbital parameters.
 
-        This method launches a GUI to allow the user to enter either TLE or direct orbital parameters (keplerian).
-        The provided data are saved into the `Orbital_Parameters` list. 
-        :param Orbital_Parameters: A list containing the global orbital parameters
-        :return: None. This function modifies the `Orbital_Parameters` list directly.
-    """    
-    label=['Major axis (a) [km] : ','Eccentricity (e) :','Inclination (i) [rad] :','RAAN [rad] :','Periapsis argument [rad] :','True anomalie (M) [rad] :','Name of the satellite: ']
-    Orbital_Parameters_Variable=['a','e','i','RAAN','OMEGA','M','name']
-    entry_window = tk.Tk()      #main window creation
-    entry_window.title('Orbital parameters selection')
+def select_data(
+    orbital_parameters: list,
+) -> None:  # function to open an user interface to give orbital parameters
+    """
+    Configures and displays the GUI to enter TLE or direct orbital parameters.
+
+    This method launches a GUI to allow the user to enter either TLE or direct orbital 
+    parameters (keplerian).
+    The provided data are saved into the `Orbital_Parameters` list.
+    :param Orbital_Parameters: A list containing the global orbital parameters
+    :return: None. This function modifies the `Orbital_Parameters` list directly.
+    """
+    label = [
+        "Major axis (a) [km] : ",
+        "Eccentricity (e) :",
+        "Inclination (i) [rad] :",
+        "RAAN [rad] :",
+        "Periapsis argument [rad] :",
+        "True anomalie (M) [rad] :",
+        "Name of the satellite: ",
+    ]
+    orbital_parameters_variable = ["a", "e", "i", "RAAN", "OMEGA", "M", "name"]
+    entry_window = tk.Tk()  # main window creation
+    entry_window.title("Orbital parameters selection")
     icone = tk.PhotoImage(file="Orbito_Logo.png")
     entry_window.iconphoto(True, icone)
 
-    #TLE
-    tles_option = tk.StringVar(value="TLE")     #TLE or parameters choice variable
+    # TLE
+    tles_option = tk.StringVar(value="TLE")  # TLE or parameters choice variable
     frame_options = tk.Frame(entry_window)
-    frame_options.pack(pady=10)      #Frame for TLE
+    frame_options.pack(pady=10)  # Frame for TLE
     label_option = tk.Label(frame_options, text="Choose an option :")
     label_option.pack(side=tk.LEFT)
-    radio_tle = tk.Radiobutton(frame_options, text="TLE", variable=tles_option, value="TLE")
+    radio_tle = tk.Radiobutton(
+        frame_options, text="TLE", variable=tles_option, value="TLE"
+    )
     radio_tle.pack(side=tk.LEFT)
-    radio_parametres = tk.Radiobutton(frame_options, text="Orbital parameters", variable=tles_option, value="Paramètres Orbitaux")
+    radio_parametres = tk.Radiobutton(
+        frame_options,
+        text="Orbital parameters",
+        variable=tles_option,
+        value="Paramètres Orbitaux",
+    )
     radio_parametres.pack(side=tk.LEFT)
     label_tle = tk.Label(entry_window, text="Please enter TLE (in two lines):")
-    label_tle.pack(pady=5) #Text box for TLE
+    label_tle.pack(pady=5)  # Text box for TLE
     entry_tle = tk.Text(entry_window, width=70, height=2)
     entry_tle.pack(pady=5)
-    entry_name_label= tk.Label(entry_window, text="Name of the satellite")
+    entry_name_label = tk.Label(entry_window, text="Name of the satellite")
     entry_name_label.pack(padx=5, pady=5)
-    entry_name_value=tk.Entry(entry_window)
+    entry_name_value = tk.Entry(entry_window)
     entry_name_value.pack(padx=5, pady=5)
 
-    #Direct ortibal parameters
+    # Direct ortibal parameters
     frame_parameters = tk.Frame(entry_window)
-    frame_parameters.pack(pady=10)    #Direct orbital parameters frame
+    frame_parameters.pack(pady=10)  # Direct orbital parameters frame
 
     for i in range(7):
-        label[i]= tk.Label(frame_parameters, text=label[i])
+        label[i] = tk.Label(frame_parameters, text=label[i])
         label[i].grid(row=i, column=0, padx=5, pady=5)
-        Orbital_Parameters_Variable[i]=tk.Entry(frame_parameters)
-        Orbital_Parameters_Variable[i].grid(row=i, column=1, padx=5, pady=5)
+        orbital_parameters_variable[i] = tk.Entry(frame_parameters)
+        orbital_parameters_variable[i].grid(row=i, column=1, padx=5, pady=5)
 
-    tles_option.trace_add("w", lambda *args: toggle_fields(tles_option, label_tle, entry_tle, entry_name_label, entry_name_value, frame_parameters))    #line to update visibility of field according to option chosen (TLE or direct parameters)
+    tles_option.trace_add(
+        "w",
+        lambda *args: toggle_fields(
+            tles_option,
+            label_tle,
+            entry_tle,
+            entry_name_label,
+            entry_name_value,
+            frame_parameters,
+        ),
+    )  # line to update visibility of field according to option chosen (TLE or direct parameters)
 
-    btn_submit = tk.Button(entry_window, text="Submit", command=lambda: submit_parameters(tles_option, entry_tle, entry_name_value,Orbital_Parameters_Variable,entry_window, Orbital_Parameters))
-    btn_submit.pack(pady=20) #Button for submission
+    btn_submit = tk.Button(
+        entry_window,
+        text="Submit",
+        command=lambda: submit_parameters(
+            tles_option,
+            entry_tle,
+            entry_name_value,
+            orbital_parameters_variable,
+            entry_window,
+            orbital_parameters,
+        ),
+    )
+    btn_submit.pack(pady=20)  # Button for submission
 
-    toggle_fields(tles_option, label_tle, entry_tle, entry_name_label, entry_name_value, frame_parameters)  # Initialize visible fields
-    entry_window.mainloop()     #Keep the user interface opened
+    toggle_fields(
+        tles_option,
+        label_tle,
+        entry_tle,
+        entry_name_label,
+        entry_name_value,
+        frame_parameters,
+    )  # Initialize visible fields
+    entry_window.mainloop()  # Keep the user interface opened
--- src/Orbital_Visualizator/Orbital_Visualizator_App.py
+++ src/Orbital_Visualizator/Orbital_Visualizator_App.py
@@ -1,42 +1,55 @@
-from Plot_Orbital_Visualizator import plot_central_body, plot_orbit
-from GUI_Orbital_Visualizator import select_data
+from plot_orbital_visualizator import plot_central_body, plot_orbit
+from gui_orbital_visualizator import select_data
 import matplotlib.pyplot as plt
 
 
-def Orbit_Visualizator(): #with entenred parameter or TLE
+def orbit_visualizator():  # with entenred parameter or TLE
     """
-        Open the tool to visualize an orbit from TLE or direct keplerian parameters.
+    Open the tool to visualize an orbit from TLE or direct keplerian parameters.
 
-        This method launches  GUI provided by the `select_data()` method allowing the user to enter orbital parameters (either TLE and direct orbital parameters).
-        It creates a 3D representation of considered orbit and the central body (e.g., Earth) using `plot_orbit()` and `plot_central_body()` methods.
+    This method launches  GUI provided by the `select_data()` method allowing the user to enter 
+    orbital parameters (either TLE and direct orbital parameters).
+    It creates a 3D representation of considered orbit and the central body (e.g., Earth) using 
+    `plot_orbit()` and `plot_central_body()` methods.
 
-        **Warning:** As this project is in its early phase of development, no validation is implemented
-        to verify the relevance or correctness of the user-provided values.
+    **Warning:** As this project is in its early phase of development, no validation is 
+    implemented to verify the relevance or correctness of the user-provided values.
 
-        :param: None.
-        :return: None. The orbit, the satellite's position and the central body are directly displayed on the created 3D object.        
+    :param: None.
+    :return: None. The orbit, the satellite's position and the central body are directly displayed
+    on the created 3D object.
     """
-    #Variables definition
-    Orbital_Parameters=[]       #under the format [a, e, i, RAAN, Omega, M,name]
+    # Variables definition
+    orbital_parameters = []  # under the format [a, e, i, RAAN, Omega, M,name]
 
-    #Figure configuration
+    # Figure configuration
     fig = plt.figure()
-    space = fig.add_subplot(111, projection='3d')
+    space = fig.add_subplot(111, projection="3d")
 
-    select_data(Orbital_Parameters)       #call user interface for data inputs
+    select_data(orbital_parameters)  # call user interface for data inputs
 
-    if not Orbital_Parameters:          #exit the function if the user interface is closed
+    if not orbital_parameters:  # exit the function if the user interface is closed
         return
 
-    lim = 1.25*Orbital_Parameters[0]     #define max range of the plot (considered 1.25 time the size of semi-major axis length)
-    space.set_xlim([-lim, lim]); space.set_ylim([-lim, lim]); space.set_zlim([-lim, lim])
+    lim = (
+        1.25 * orbital_parameters[0]
+    )  # define max range of the plot (considered 1.25 time the size of semi-major axis length)
+    space.set_xlim([-lim, lim])
+    space.set_ylim([-lim, lim])
+    space.set_zlim([-lim, lim])
 
-    space.set_xlabel('X in km'); space.set_ylabel('Y in km'); space.set_zlabel('Z in km')
+    space.set_xlabel("X in km")
+    space.set_ylabel("Y in km")
+    space.set_zlabel("Z in km")
 
-    space.set_title('Orbit visualization around Earth of ' +str(Orbital_Parameters[6])+ ' in EQJ2000 frame')
+    space.set_title(
+        "Orbit visualization around Earth of "
+        + str(orbital_parameters[6])
+        + " in EQJ2000 frame"
+    )
     space.set_box_aspect([1, 1, 1])
 
-    plot_orbit(space, Orbital_Parameters)
-    plot_central_body(space, radius=6371, color='b') #plot of the central body
+    plot_orbit(space, orbital_parameters)
+    plot_central_body(space, radius=6371, color="b")  # plot of the central body
 
-    plt.show() #Show space with the central body and orbit considered
+    plt.show()  # Show space with the central body and orbit considered
