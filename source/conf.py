a34521eb1fe83bb6fcb843ceea2b556c147304fe
Baptistestaca <81914248+Baptistestaca@users.noreply.github.com>
2024-12-16 05:11:41 +0100

    Rebuilt html

--- source/conf.py
+++ source/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -5,6 +9,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+sys.path.insert(0, os.path.abspath('../my_project'))  # Ajuste le chemin selon ton projet
+
 
 project = 'Orbital_Visualizator'
 copyright = '2024, Baptiste LEBON'
@@ -14,15 +21,18 @@ release = '16/12/2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
 
-templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
